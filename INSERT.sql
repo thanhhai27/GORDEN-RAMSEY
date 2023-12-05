5d0582d8bbc16f3f47a773a44db1c85a667a5c84
nguyenhai-fire273 <113079282+nguyenhai-fire273@users.noreply.github.com>
2023-12-05 14:34:22 +0700

    CREATE

---
+++ INSERT.sql
@@ -0,0 +1,34 @@
+INSERT INTO KHACHHANG VALUES
+('056434565432','Nguyen Thanh Tung','Nam','1994-07-05','thanhtung123@gmail.com',150,12,'Dien Bien Phu','Phuong 21','Quan Binh Thanh','TP Ho Chi Minh'),
+('032435434231','Nguyen Thuc Thuy Tien','Nu','1998-08-12','ldbl1208@gmail.com',200,44,'Ly Nam De','Phuong 7','Quan 11','TP Ho Chi Minh'),
+('046454234645','Huynh Tran Thanh','Nam','1987-02-05','tranthanh111@gmail.com',500,15,'Cach Mang Thang 8','Phuong 11','Quan 3','TP Ho Chi Minh'),
+('098762345687','Vo Vu Truong Giang','Nam','1983-04-20','vvtruonggiang1412@gmail.com',50,90,'Ly Nam De','Phuong 7','Quan 11','TP Ho Chi Minh'),
+('045646756343','Phan Thi My Tam','Nu','1981-01-16','mytam1001@gmail.com',666,10,'Ly Thuong Kiet','Phuong 7','Quan 10','TP Ho Chi Minh'),
+('023453562453','Nguyen Thi Hoa','Nu','1995-05-31','hoaminzy113@gmail.com',123,30,'Su Van Hanh','Phuong 12', 'Quan 10','TP Ho Chi Minh'),
+('023453142545','Ong Cao Thang','Nam','1986-01-13','ongthang99@gmail.com',589,83,'Dien Bien Phu','Phuong 15','Quan Binh Thanh','TP Ho Chi Minh'),
+('019384583921','Nghiem Vu Hoang Long','Nam','1999-03-02','mckakanger@gmail.com',340,34,'Ly Nam De','Phuong Hoc Mon','Quan Hoc Mon','TP Ho Chi Minh'),
+('023929452342','Nguyen Khoa Toc Tien','Nu','1989-05-13','toctienne123@gmail.com',104,11,'Su Van Hanh','Phuong 10','Quan 10','TP Ho Chi Minh'),
+('020425929592','Phung Thanh Do','Nam','1989-09-12','domiximoi100@gmail.com',129,99,'Ngo Gia Tu','Phuong 2','Quan 10','TP Ho Chi Minh')
+
+INSERT INTO TAIKHOAN VALUES
+('134135','thanhtung123','Thanhtung@123','056434565432'),
+('235346','thuytien109','Tien1208','032435434231'),
+('314325','thanhtran111','05021987','046454234645'),
+('429310','giangson001','Son123456','098762345687'),
+('523634','casimytam11','19810116','045646756343'),
+('636241','hoaminzy90','Hoahoahoa31','023453562453'),
+('745634','thangcaoong1','Thangco123','023453142545'),
+('845432','mckakalongnger','Hoanglong1999','019384583921'),
+('913042','toctien102','Tiengiangtran01','023929452342'),
+('103030','domixi888','Mixifood1209','020425929592')
+
+-- INSERT INTO VOUCHER VALUES
+-- ('NH1000K',NULL,NULL,NULL,'1000000','2023-10-20','2023-12-29'),
+-- ('NH200XYZ','Sup to yen hat sen,Xuc xich bo Kobe nuong','5 lon bia Heineken',NULL,NULL,'2023-01-02','2023-02-02'),
+-- ('NHANTHAGA','Loi vai bo My nuong, Sup miso, Hau chien gion, Bap gio heo xong khoi',NULL,'Giam 500000(Hoa don tren 3)',NULL,'2023-04-10','2023-04-17'),
+-- ('NHANUONG','Xoi bo cau, 1 nuoc ep cam, 1 bia Heineken Ha Lan','2 Gau Bong',NULL,'5%',NULL,'2023-11-30','2023-12-5'),
+-- ('NHABCDEF',,,,,'2023-12-30','2024-01-01'),
+-- ('NH2000K',,,,,'2023-07-09','2023-10-01')
+
+INSERT INTO FOODANDDRINK VALUES
+('Sup Hai San','Version 1',150000,'Tom Alaska, cua Tuyet,rau ngo,toi,hanh tim,nam huong,ca rot,muoi,tieu,ca chua','...')
