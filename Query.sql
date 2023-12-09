909f9510b1961c3fcaf80214d21daf9f5a63a9bd
Bil <ptbinh804@gmail.com>
2023-12-09 21:28:16 +0700

    Bil

--- Query.sql
+++ Query.sql
@@ -12,17 +12,5 @@ CREATE TRIGGER trig_update_food_and_drink ON FOODANDDRINK
 INSTEAD OF UPDATE 
 AS
 BEGIN
-	DECLARE @version INT, 
-			@ten VARCHAR(100), 
-			@gia BIGINT, 
-			@nguyenlieu VARCHAR(100), 
-			@mota VARCHAR(100), 
-			@ngaythem DATE, 
-			@tinhtrang VARCHAR(100)
-	SELECT @ten = TEN, @gia = GIA	
-	FROM inserted	
-	IF UPDATE (GIA)
-	BEGIN
-		
-	END
+	
 END
