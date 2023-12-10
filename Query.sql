36f491a0d09e234cc07ef10a4b62d7238b4697c6
Bil <ptbinh804@gmail.com>
2023-12-10 17:01:59 +0700

    Bil

--- Query.sql
+++ Query.sql
@@ -9,8 +9,69 @@ SELECT * FROM Menu;
 GO
 
 CREATE TRIGGER trig_update_food_and_drink ON FOODANDDRINK
-INSTEAD OF UPDATE 
+FOR UPDATE
 AS
 BEGIN
-	
+    IF UPDATE(GIA)
+    BEGIN
+        DECLARE @version INT,
+                @ten VARCHAR(100),
+                @gia BIGINT,
+                @loai VARCHAR(100),
+                @nguyenlieu VARCHAR(100),
+                @mota VARCHAR(100),
+                @ngaythem DATE,
+                @tinhtrang VARCHAR(100),
+                @xuatxu VARCHAR(100),
+                @cachchebien VARCHAR(100),
+                @canhbaodiung VARCHAR(100)
+
+        SELECT  @ten = TEN
+        FROM inserted
+
+        -- Xac dinh loai
+        IF (@ten IN 
+                    (
+                        SELECT TEN
+                        FROM MON_AN
+                    )
+            )
+        BEGIN
+            SET @loai = 'DO AN'
+        END
+        ELSE
+        BEGIN
+            SET @loai = 'DO UONG'
+        END
+
+        -- Lay du lieu gia va ngay them cu
+        SELECT  @gia = GIA
+                @ngaythem = NGAYTHEM
+        FROM deleted
+
+        -- Lay du lieu them moi
+        SELECT  @nguyenlieu = NGUYENLIEU,
+                @mota = MOTA
+        FROM inserted
+
+        -- Tinh version
+        SET @version = (
+                            SELECT MAX(VERSION)
+                            FROM FOODANDDRINK
+                            WHERE TEN = @ten
+                        ) + 1
+
+        -- Thiet lap tinh trang
+        SET @tinhtrang = 'Khong ban'
+
+        -- Thiet lap ngay them moi
+        UPDATE TABLE FOODANDDRINK
+        SET NGAYTHEM = CONVERT(datetime, GETDATE(), 111)
+        WHERE TEN = @ten AND TINHTRANG = 'Con ban'
+
+        -- Chen du lieu moi
+        INSERT INTO FOODANDDRINK
+        VALUES (@ten, @version, @gia, @nguyenlieu, @mota, @ngaythem, @tinhtrang)
+        END
+    END
 END
